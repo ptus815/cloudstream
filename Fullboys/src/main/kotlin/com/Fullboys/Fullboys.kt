61f88d84bd3a315ad2118bc5d1a2256e1ea140a5
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-05 20:54:33 +0700

    Update Fullboys.kt

--- Fullboys/src/main/kotlin/com/Fullboys/Fullboys.kt
+++ Fullboys/src/main/kotlin/com/Fullboys/Fullboys.kt
@@ -2,6 +2,9 @@ package com.Fullboys
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.nodes.Element
 
 class Fullboys : MainAPI() {
     override var mainUrl              = "https://fullboys.com"
