93f5b3902bc4e9fbc968a872fca7dd7362a8cac8
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-07 17:32:43 +0700

    Update FullboysProvider.kt

--- Fullboys/src/main/kotlin/com/Fullboys/FullboysProvider.kt
+++ Fullboys/src/main/kotlin/com/Fullboys/FullboysProvider.kt
@@ -3,6 +3,10 @@ package com.Fullboys
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+plugins {
+    id("com.lagradost.cloudstream.plugin") version "1.7.0"  // ✅ dòng mới
+}
+
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.DoodPmExtractor
 
