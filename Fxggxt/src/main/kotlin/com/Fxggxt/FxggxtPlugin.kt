52a5f61b28e5578403bc4f58b22dd41af4bf5a0b
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-09 16:43:01 +0700

    Update FxggxtPlugin.kt

---
+++ Fxggxt/src/main/kotlin/com/Fxggxt/FxggxtPlugin.kt
@@ -0,0 +1,16 @@
+package com.Fxggxt
+
+import android.content.Context
+import com.Fxggxt.Fxggt
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.StreamTape
+
+@CloudstreamPlugin
+class FxggxtPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Fxggxt())
+        registerExtractorAPI(StreamTape())
+
+    }
+}
