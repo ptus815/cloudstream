69f5c37167f1c7bc68dba07feefb0e7abeef3f5c
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-09 16:53:26 +0700

    Update FxggxtPlugin.kt

--- Fxggxt/src/main/kotlin/com/Fxggxt/FxggxtPlugin.kt
+++ Fxggxt/src/main/kotlin/com/Fxggxt/FxggxtPlugin.kt
@@ -1,7 +1,7 @@
 package com.Fxggxt
 
 import android.content.Context
-import com.Fxggxt.Fxggt
+import com.Fxggxt.Fxggxt
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.extractors.StreamTape
