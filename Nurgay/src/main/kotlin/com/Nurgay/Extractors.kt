9b1b55440ad3af0ac5fa91fe51bc596db58f55a6
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-10 21:48:58 +0700

    Update Extractors.kt

--- Nurgay/src/main/kotlin/com/Nurgay/Extractors.kt
+++ Nurgay/src/main/kotlin/com/Nurgay/Extractors.kt
@@ -1,33 +1,55 @@
 package com.Nurgay
 
-import org.jsoup.nodes.Element
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.extractors.Vidguardto
+import com.lagradost.cloudstream3.extractors.VidhideExtractor
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.cloudstream3.utils.newExtractorLink
-import com.lagradost.cloudstream3.extractors.Voe
-import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
-class Dooodster : DoodLaExtractor() {
-    override var mainUrl = "https://dooodster.com"
+class Vidsp : VidhideExtractor() {
+    override var mainUrl = "https://vidsp.lol"
 }
 
-class Listeamed : Vidguardto() {
-    override var mainUrl = "https://listeamed.net"
+class VidHideplus : VidhideExtractor() {
+    override var mainUrl = "https://vidhideplus.com"
+}
+class VidHidedht: VidhideExtractor() {
+    override var mainUrl = "https://dhtpre.com"
 }
 
-class Beamed : Vidguardto() {
-    override var mainUrl = "https://bembed.net"
+class Vidhidehub : VidhideExtractor() {
+    override var mainUrl = "https://vidhidehub.com"
 }
 
-class StreamTapeto : StreamTape() {
-    override var mainUrl = "https://streamtape.to"
+open class Ds2play : ExtractorApi() {
+    override var name = "DoodStream"
+    override var mainUrl = "https://ds2play.com"
+    override val requiresReferer = false
+
+    override fun getExtractorUrl(id: String): String {
+        return "https://dood.wf/d/$id"
+    }
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+        val response0 = app.get(url).text // html of DoodStream page to look for /pass_md5/...
+        val md5 =mainUrl+(Regex("/pass_md5/[^']*").find(response0)?.value ?: return null)  // get https://dood.ws/pass_md5/...
+        val trueUrl = app.get(md5, referer = url).text + "zUEJeL3mUN?token=" + md5.substringAfterLast("/")   //direct link to extract  (zUEJeL3mUN is random)
+        val quality = Regex("\\d{3,4}p").find(response0.substringAfter("<title>").substringBefore("</title>"))?.groupValues?.get(0)
+        return listOf(
+            newExtractorLink(
+                source = this.name,
+                name = this.name,
+                url = trueUrl
+            ) {
+                this.referer = mainUrl
+                this.quality = getQualityFromName(quality)
+            }
+        ) // links are valid in 8h
+
+    }
 }
 
 open class Bigwarp : ExtractorApi() {
@@ -39,11 +61,10 @@ open class Bigwarp : ExtractorApi() {
         url: String,
         referer: String?,
     ): List<ExtractorLink>? {
-        return try {
-            val response = app.get(url).document
-            val script = response.selectFirst("script:containsData(sources)")?.data().toString()
-            Regex("sources:\\s*\\[.*file:\"(.*)\".*").find(script)?.groupValues?.get(1)?.let { link ->
-                listOf(
+        val response =app.get(url).document
+        val script = response.selectFirst("script:containsData(sources)")?.data().toString()
+        Regex("sources:\\s*\\[.file:\"(.*)\".*").find(script)?.groupValues?.get(1)?.let { link ->
+                return listOf(
                     newExtractorLink(
                         source = name,
                         name = name,
@@ -54,10 +75,7 @@ open class Bigwarp : ExtractorApi() {
                         this.quality = Qualities.P1080.value
                     }
                 )
-            }
-        } catch (e: Exception) {
-            Log.e(e) // Log error for debugging
-            null
         }
+        return null
     }
 }
