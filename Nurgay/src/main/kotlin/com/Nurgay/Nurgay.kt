4ed917c8b62400381f3f452a503d2a861a993bf6
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-10 22:11:21 +0700

    Update Nurgay.kt

--- Nurgay/src/main/kotlin/com/Nurgay/Nurgay.kt
+++ Nurgay/src/main/kotlin/com/Nurgay/Nurgay.kt
@@ -1,17 +1,19 @@
 package com.Nurgay
 
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import org.jsoup.nodes.Element
+import android.annotation.SuppressLint
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.Jsoup
+import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.lagradost.api.Log
+import org.jsoup.nodes.Element
+import java.time.Year
 
 class Nurgay : MainAPI() {
     override var mainUrl = "https://nurgay.to"
@@ -27,81 +29,97 @@ class Nurgay : MainAPI() {
         "?filter=most-viewed" to "Most Viewed"
     )
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        return try {
-            val url = if (page == 1) "$mainUrl/${request.data}" else "$mainUrl/${request.data}/page/$page/"
-            val doc = app.get(url).document
-            val items = doc.select("div.thumb-block").mapNotNull { it.toSearchResult() }
+override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val url = if(page == 1) "$mainUrl/${request.data}/" else "$mainUrl/${request.data}/page/$page/"
+        val document = app.get(url).document
+        val home =
+            document.select("div.items.normal article, div#archive-content article, div.items.full article").mapNotNull {
+                it.toSearchResult()
+            }
+        return newHomePageResponse(request.name, home)
+    }
 
-            newHomePageResponse(
-                HomePageList(name = request.name, list = items),
-                hasNext = items.isNotEmpty()
-            )
-        } catch (e: Exception) {
-            Log.e(e) // Log error for debugging
-            newHomePageResponse(HomePageList(name = request.name, list = emptyList()), hasNext = false)
+    private fun getProperLink(uri: String): String {
+        return when {
+            uri.contains("/episodes/") -> {
+                var title = uri.substringAfter("$mainUrl/episodes/")
+                title = Regex("(.+?)-season").find(title)?.groupValues?.get(1).toString()
+                "$mainUrl/tvshows/$title"
+            }
+
+            uri.contains("/seasons/") -> {
+                var title = uri.substringAfter("$mainUrl/seasons/")
+                title = Regex("(.+?)-season").find(title)?.groupValues?.get(1).toString()
+                "$mainUrl/tvshows/$title"
+            }
+
+            else -> {
+                uri
+            }
         }
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val aTag = selectFirst("a") ?: return null
-        val href = fixUrl(aTag.attr("href"))
-        val title = aTag.selectFirst(".entry-header .video-title")?.text()?.trim() ?: return null
-        val image = aTag.selectFirst("img")?.attr("src") ?: return null
+        val title = this.selectFirst("h3 > a")?.text() ?: return null
+        val href = getProperLink(fixUrl(this.selectFirst("h3 > a")!!.attr("href")))
+        var posterUrl = this.select("div.poster img").last()?.getImageAttr()
+        if (posterUrl != null) {
+            if (posterUrl.contains(".gif")) {
+                posterUrl = fixUrlNull(this.select("div.poster img").attr("data-wpfc-original-src"))
+            }
+        }
+        val quality = getQualityFromString(this.select("span.quality").text())
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+            this.quality = quality
+        }
 
-        return MovieSearchResponse(
-            name = title,
-            url = href,
-            apiName = this@Nurgay.name,
-            type = TvType.NSFW,
-            posterUrl = image
-        )
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        return try {
-            val url = "$mainUrl/?s=$query"
-            val doc = app.get(url).document
-            doc.select("div.thumb-block").mapNotNull { it.toSearchResult() }
-        } catch (e: Exception) {
-            Log.e(e) // Log error for debugging
-            emptyList()
+        val document = app.get("$mainUrl/search/$query").document
+        return document.select("div.result-item").map {
+            val title =
+                it.selectFirst("div.title > a")!!.text().replace(Regex("\\(\\d{4}\\)"), "").trim()
+            val href = getProperLink(it.selectFirst("div.title > a")!!.attr("href"))
+            val posterUrl = it.selectFirst("img")!!.attr("src")
+            newMovieSearchResponse(title, href, TvType.TvSeries) {
+                this.posterUrl = posterUrl
+            }
         }
     }
 
     override suspend fun load(url: String): LoadResponse {
-        return try {
-            val request = app.get(url)
-            val document = request.document
-            val title = document.selectFirst("div.data > h1")?.text()?.trim().toString()
-            var posterUrl = fixUrlNull(document.selectFirst("meta[property=og:image]")?.attr("content"))
-            if (posterUrl.isNullOrEmpty()) {
+        val request = app.get(url)
+        val document = request.document
+        val title =
+            document.selectFirst("div.data > h1")?.text()?.trim().toString()
+        var posterUrl = fixUrlNull(document.selectFirst("meta[property=og:image]")?.attr("content"))
+        if (posterUrl.isNullOrEmpty()) {
                 posterUrl = fixUrlNull(document.select("div.poster img").attr("src"))
-            }
-            val description = document.select("div.wp-content > p").text().trim()
-            val episodes = document.select("ul#playeroptionsul > li").map {
-                val name = it.selectFirst("span.title")?.text()
-                val type = it.attr("data-type")
-                val post = it.attr("data-post")
-                val nume = it.attr("data-nume")
-                Episode(
-                    LinkData(type, post, nume).toJson(),
-                    name,
-                )
-            }
+        }
+        val description = document.select("div.wp-content > p").text().trim()
+            val episodes =
+                document.select("ul#playeroptionsul > li").map {
+                    val name = it.selectFirst("span.title")?.text()
+                    val type = it.attr("data-type")
+                    val post = it.attr("data-post")
+                    val nume = it.attr("data-nume")
+                    Episode(
+                        LinkData(type, post,nume).toJson(),
+                        name,
+                    )
+                }
 
-            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+            return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
                 this.posterUrl = posterUrl
                 this.plot = description
             }
-        } catch (e: Exception) {
-            Log.e(e) // Log error for debugging
-            newTvSeriesLoadResponse("Unknown Title", url, TvType.TvSeries, emptyList()) {
-                this.posterUrl = null
-                this.plot = "No description available."
-            }
         }
-    }
+
 
     override suspend fun loadLinks(
         data: String,
@@ -109,24 +127,42 @@ class Nurgay : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        return try {
             val loadData = tryParseJson<LinkData>(data)
             val source = app.post(
                 url = "$mainUrl/wp-admin/admin-ajax.php", data = mapOf(
                     "action" to "doo_player_ajax", "post" to "${loadData?.post}", "nume" to "${loadData?.nume}", "type" to "${loadData?.type}"
                 ), referer = data, headers = mapOf("Accept" to "*/*", "X-Requested-With" to "XMLHttpRequest"
-                )
-            ).parsed<ResponseHash>().embed_url.getIframe()
+                )).parsed<ResponseHash>().embed_url.getIframe()
             if (!source.contains("youtube")) loadExtractor(
                 source,
                 "$directUrl/",
                 subtitleCallback,
                 callback
             )
-            true
-        } catch (e: Exception) {
-            Log.e(e) // Log error for debugging
-            false
+        return true
+        }
+
+    private fun Element.getImageAttr(): String {
+        return when {
+            this.hasAttr("data-src") -> this.attr("abs:data-src")
+            this.hasAttr("data-lazy-src") -> this.attr("abs:data-lazy-src")
+            this.hasAttr("srcset") -> this.attr("abs:srcset").substringBefore(" ")
+            else -> this.attr("abs:src")
         }
     }
-}
+
+    private fun String.getIframe(): String {
+        return Jsoup.parse(this).select("iframe").attr("src")
+    }
+
+    data class LinkData(
+        val type: String? = null,
+        val post: String? = null,
+        val nume: String? = null,
+    )
+
+    data class ResponseHash(
+        @JsonProperty("embed_url") val embed_url: String,
+        @JsonProperty("type") val type: String?,
+    )
+}    
