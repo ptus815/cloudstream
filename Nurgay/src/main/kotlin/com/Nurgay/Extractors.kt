59448617cd8603bc919241926747cb6607976499
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-10 12:46:18 +0700

    Update Extractors.kt

--- Nurgay/src/main/kotlin/com/Nurgay/Extractors.kt
+++ Nurgay/src/main/kotlin/com/Nurgay/Extractors.kt
@@ -3,8 +3,13 @@ package com.Nurgay
 import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.extractors.Vidguardto
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.extractors.Voe
 
 class Dooodster : DoodLaExtractor() {
     override var mainUrl = "https://dooodster.com"
@@ -16,6 +21,10 @@ class Listeamed : Vidguardto() {
 
 class Beamed : Vidguardto() {
     override var mainUrl = "https://bembed.net"
+}
+
+class MixDropAG : MixDrop(){
+    override var mainUrl = "https://mixdrop.ag"
 }
 
 class Bgwp : Bigwarp() {
